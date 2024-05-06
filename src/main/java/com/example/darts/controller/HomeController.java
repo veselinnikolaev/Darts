eeb80acdb8a4a057fb7e1ddbe7a98ab71ce9de65
Veselin Nikolaev <vslnnikolaev@gmail.com>
2024-05-07 01:00:19 +0300

    removed spring security + adjusted folders. problem with static resources

--- src/main/java/com/example/darts/controller/HomeController.java
+++ src/main/java/com/example/darts/controller/HomeController.java
@@ -10,4 +10,44 @@ public class HomeController {
     public ModelAndView index() {
         return new ModelAndView("index");
     }
+
+    @GetMapping("/about")
+    public ModelAndView about() {
+        return new ModelAndView("about");
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView contact() {
+        return new ModelAndView("contact");
+    }
+
+    @GetMapping("/blog")
+    public ModelAndView blog() {
+        return new ModelAndView("blog");
+    }
+
+    @GetMapping("/elements")
+    public ModelAndView elements() {
+        return new ModelAndView("elements");
+    }
+
+    @GetMapping("/jobs")
+    public ModelAndView jobs() {
+        return new ModelAndView("job_listing");
+    }
+
+    @GetMapping("/jobs/details")
+    public ModelAndView jobDetails() {
+        return new ModelAndView("job_details");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView main() {
+        return new ModelAndView("main");
+    }
+
+    @GetMapping("/blog/single")
+    public ModelAndView singleBlog() {
+        return new ModelAndView("single-blog");
+    }
 }
